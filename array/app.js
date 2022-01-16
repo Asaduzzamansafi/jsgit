56a772a516450d0616257d475948304a40f307f7
Safi <asaduzzamansafi@gmail.com>
2022-01-16 21:04:51 +0600

    length

--- array/app.js
+++ array/app.js
@@ -1,2 +1,15 @@
-let team7 = ["kakashi","naruto","sasuke","sakura"]
-document.write(team7[1 `+` 2]);
+let team7 = ["kakashi","naruto","sasuke","sakura"];
+
+document.write(team7[1]);
+
+document.write("<br>")
+
+// for (let a of team7){
+//     document.write("a");
+// }
+
+document.write(team7.length); 
+
+document.write("<br>")
+
+document.write(team7[team7.length-2]); 
