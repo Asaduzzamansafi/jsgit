58cf62abe20f2d915ec43348d70e2877d8b3da5a
Safi <asaduzzamansafi@gmail.com>
2022-01-17 18:53:09 +0600

    sai and yamato added in the in team

--- array/app.js
+++ array/app.js
@@ -2,17 +2,30 @@ let team7 = ["kakashi","naruto","sasuke","sakura",7];
 
 document.write(team7[1]);
 
-document.write("<br>")
+document.write("<br>");
 
 document.write(team7.length); 
 
-document.write("<br>")
+document.write("<br>");
 
 document.write(team7[team7.length-2]);
 
-document.write("<br>")
+document.write("<br>");
 
 // for Each 
 team7.forEach(function(element,index){
     document.write(element + index +" ");
 });
+
+document.write("<br>");
+
+document.write(team7 + "<br>");
+
+
+team7.push("sai");
+
+document.write(team7 + "<br>");
+
+team7.unshift("yamato");
+
+document.write(team7 + "<br>");
