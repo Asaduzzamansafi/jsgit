7ae78b2e3c0c43707a9103243b9708f64791749b
Safi <asaduzzamansafi@gmail.com>
2022-01-17 18:27:17 +0600

    for Each added

--- array/app.js
+++ array/app.js
@@ -1,15 +1,18 @@
-let team7 = ["kakashi","naruto","sasuke","sakura"];
+let team7 = ["kakashi","naruto","sasuke","sakura",7];
 
 document.write(team7[1]);
 
 document.write("<br>")
 
-// for (let a of team7){
-//     document.write("a");
-// }
-
 document.write(team7.length); 
 
 document.write("<br>")
 
-document.write(team7[team7.length-2]); 
+document.write(team7[team7.length-2]);
+
+document.write("<br>")
+
+// for Each 
+team7.forEach(function(element,index){
+    document.write(element + index +" ");
+});
