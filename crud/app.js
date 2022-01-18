d2896747bff1b3f2b6d6aee8196911280486a252
Safi <asaduzzamansafi@gmail.com>
2022-01-18 19:28:06 +0600

    splice added

---
+++ crud/app.js
@@ -0,0 +1,10 @@
+// CRUD = (Creat Read Update Delete)
+let color = ["red","green","blue","orange"];
+
+document.write(color);
+
+color.splice(1,1);
+
+document.write("<br>");
+
+document.write(color);
