8e4179ed6b9004e6f6cc54459e37784ae8e58ecc
Safi <asaduzzamansafi@gmail.com>
2022-01-18 19:38:13 +0600

    data deleted and updated

--- crud/app.js
+++ crud/app.js
@@ -3,8 +3,22 @@ let color = ["red","green","blue","orange"];
 
 document.write(color);
 
+// splice = (index,delet,data,update)
+
 color.splice(1,1);
 
 document.write("<br>");
+
+document.write(color);
+
+document.write("<br>");
+
+color.splice(0,0,"lime");
+
+document.write(color);
+
+document.write("<br>");
+
+color.splice(1,1,"pink");
 
 document.write(color);
